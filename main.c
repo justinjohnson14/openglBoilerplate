8ee0b64ae3b094f37012fe2b0174d695533ecd78
Justin Johnson <justinjohnson14@outlook.com>
2024-10-16 17:51:37 -0700

    Setup simple structure for starting point

--- main.c
+++ main.c
@@ -1,27 +1,11 @@
-#include <GL/glew.h>
+#include "main.h"
 #include <GLFW/glfw3.h>
-#include <stdio.h>
 
-void error_callback(int error, const char* description)
-{
-
-}
-
-void render()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    GLfloat vertices[] = {
+GLfloat vertices[] = {
         0.0f, 0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f
     };
-
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glVertexPointer(3, GL_FLOAT, 0, vertices);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDisableClientState(GL_VERTEX_ARRAY);
-}
 
 int main()
 {
@@ -51,15 +35,81 @@ int main()
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-    while(!glfwWindowShouldClose(window))
-    {
-        render();
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+    run(window);
 
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
+}
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void render()
+{
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    //Draw step
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    //Unbind the VAO and VBO (optional for safety)
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void run(GLFWwindow* window)
+{
+    double previous = getCurrentTime();
+    double lag = 0.0;
+    while(!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        double current = getCurrentTime();
+        double elapsed = current - previous;
+        previous = current;
+        lag += elapsed;
+
+        processInput();
+
+        while(lag >= MS_PER_UPDATE)
+        {
+            update();
+            lag -= MS_PER_UPDATE;
+        }
+
+        render();
+
+        glfwSwapBuffers(window);
+    }
+}
+
+void update()
+{
+
+}
+
+void processInput()
+{
+    glfwPollEvents();
+}
+
+double getCurrentTime()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec / 1e9;
 }
